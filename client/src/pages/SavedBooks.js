3bd963e303a74abf77b9e805ac273a5090382f9f
Matthew Wiessing <matthewwiessing@gmail.com>
2022-01-03 16:47:38 -0600

    fixed same typo that was in search book.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -10,7 +10,7 @@ import {
 import { useQuery, useMutation } from "@apollo/client";
 
 import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutations";
+import { REMOVE_BOOK } from "../utils/mutation";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
